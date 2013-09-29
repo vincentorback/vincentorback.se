4e32b9eb8eb9b5836f379111575493d31b504225
Vincent Orback <vorback@gmail.com>
2013-09-29 23:02:32 +0200

    Update more grunt stuff

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,29 +5,9 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     
     watch: {
-	  options: {
-	    livereload: true
-	  },
 	  js: {
 	    files: ["js/*.js"],
 	    tasks: "uglify"
-	  },
-	  sass: {
-		  files: ["sass/**/*.scss"],
-		  tasks: ["compass"]
-	  },
-	  css: {
-	    files: ["*.css"]
-	  },
-	  imageoptim: {
-		  files: ["images/**/*.jpg", "images/**/*.png"],
-		  taskes: "imageoptim"
-	  },
-	  livereload: {
-		  files: ["*.css"],
-		  options: {
-			  livereload: true
-		  }
 	  }
 	},
     uglify: {
@@ -40,27 +20,15 @@ module.exports = function(grunt) {
 	      	'js/main-min.js': ['js/jquery-1.9.1.min.js', 'js/modernizr.js', 'js/resize.js', 'js/main.js']
       	}
       }
-    },
-    imageoptim: {
-	  files: ["images"]
-    },
-    compass: {
-    	default: {
-	    	options: {
-			    sassDir: 'sass',
-			    config: 'config.rb'
-		    }
-	    }
     }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-imageoptim');
   grunt.loadNpmTasks('grunt-contrib-compass');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task(s).
-  grunt.registerTask('default', ['compass','uglify','imageoptim','watch']);
+  grunt.registerTask('default', ['uglify','watch']);
 
 };
