77fe2a17e3c30ef15f47bb13fc28dfd004eee40f
Vincent Orback <vorback@gmail.com>
2013-10-03 18:51:45 +0200

    Minor nav updates

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,34 +1,32 @@
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    
-    watch: {
-	  js: {
-	    files: ["js/*.js"],
-	    tasks: "uglify"
-	  }
-	},
-    uglify: {
-      options: {
-        banner: '/*! Vincent Orback <%= grunt.template.today("yyyy-mm-dd") %> */\n',
-        mangle: true
-      },
-      my_target: {
-      	files: {
-	      	'js/main-min.js': ['js/jquery-1.9.1.min.js', 'js/modernizr.js', 'js/resize.js', 'js/main.js']
-      	}
-      }
-    }
-  });
+	grunt.initConfig({
 
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-compass');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+		pkg: grunt.file.readJSON('package.json'),
 
-  // Default task(s).
-  grunt.registerTask('default', ['uglify','watch']);
+		watch: {
+			js: {
+				files: ["js/*.js"],
+				tasks: "uglify"
+			}
+		},
 
+		uglify: {
+			options: {
+				banner: '/*! Vincent Orback <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+				mangle: true
+			},
+			my_target: {
+				files: {
+					'js/main-min.js': ['js/jquery-1.9.1.min.js', 'js/modernizr.js', 'js/resize.js', 'js/main.js']
+				}
+			}
+		}
+
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+
+	grunt.registerTask('default', ['uglify', 'watch']);
 };
