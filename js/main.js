1069dba5f3d642117260ad3cc72c2276209f92d7
Vincent Orback <vorback@gmail.com>
2013-10-05 18:13:15 +0200

    Updated the contact form with better validation

--- js/main.js
+++ js/main.js
@@ -1,151 +1,97 @@
-var winWidth = $(window).width(), winHeight = $(window).height();
+var winWidth = $(window).width();
 
 var vincent = {
-	init : function() {
-		if( $('#portfolio').is('main') ) {
+	
+	init : function () {
+		if ($('#portfolio').is('main')) {
 			vincent.smoothScroll();
 		}
-		if( $('#contact').is('section') ) {
+		if ($('#contact').is('section')) {
+			vincent.contactForm();
 			vincent.inputGrow();
 		}
 	},
-	smoothScroll : function() {
-		if(winWidth > 1140){
-			var scrollDistance = 235;
+	smoothScroll: function () {
+		var scrollDistance;
+
+		if (winWidth > 1140) {
+			scrollDistance = 235;
 		}
 		else {
-			var scrollDistance = 30;
+			scrollDistance = 30;
 		}
-		$(".scroll a").bind("click", function(event) {
+		$(".scroll a").bind("click", function (e) {
 			$("html, body").stop().animate({
 				scrollTop: $($(this).attr("href")).offset().top + scrollDistance
 			}, 900);
-			event.preventDefault();
+			e.preventDefault();
 		});
 	},
-	inputGrow : function() {
-		$('#contact-form, #en-contact-form').submit(function(e){
+	contactForm: function () {
+		$('#contact-form').on('submit', function (e) {
 			e.preventDefault();
-			var form = $(this);
-			var post_url = form.attr('action');
-			var post_data = form.serialize();
-			var name = $('input[name=name]');
-			var email = $('input[name=email]');
-			var message = $('textarea[name=message]');
-			$('body,html').animate({
-				scrollTop: 330
-			}, 300);
-			if (name.val()==='') {
-				return false;
+
+			var form = $(this),
+				post_url = form.attr('action'),
+				post_data = form.serialize(),
+				name = $('#name'),
+				email = $('#email'),
+				message = $('#message'),
+				emptyName = "Vad heter du?",
+				emptyEmail = "Fyll i en riktigt e-post!",
+				emptyMessage = "Vad var det du ville säga?";
+			if (form.hasClass('english')) {
+				emptyName = "What's your name?";
+				emptyEmail = "Please enter a valid e-mail!";
+				emptyMessage = "What did you come here to say";
 			}
-			if (email.val()==='') {
-				return false;
+
+			$('body, html').animate({scrollTop: 330}, 300);
+
+			if (name.val() === "") {
+				name.addClass("needsfilled");
+				name.attr("placeholder", emptyName);
 			}
-			if (message.val()==='') {
-				return false;
+			if (!/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/.test(email.val())) {
+				email.addClass("needsfilled");
+				email.attr("placeholder", emptyEmail);
 			}
+			if (message.val() === "") {
+				message.addClass("needsfilled");
+				message.attr("placeholder", emptyMessage);
+			}
+			if ($(":input").hasClass("needsfilled")) {
+				return false;
+			} else {
+				return true;
+			}
+
 			$.ajax({
 				type: 'POST',
 				url: post_url,
 				data: post_data,
-				success: function(msg) {
-					$(form).fadeOut(1000, function(){
+				success: function (msg) {
+					form.fadeOut(1000, function () {
 						form.html(msg).fadeIn("slow");
 					});
 				}
 			});
+
 		});
-		var required = ["name", "email", "message"];
-		var name = $("#name");
-		var email = $("#email");
-		var message = $("#message");
-		var enemptyerror = "Fill this form would you kindly?";
-		var ennameerror = "What's your name?";
-		var enemailerror = "Please enter a valid e-mail.";
-		var enmessageerror = "What did you come here to say?";
-		var emptyerror = "Fyll i det här fältet tack!";
-		var nameerror = "Vad heter du?";
-		var emailerror = "Fyll i en riktig e-postadress!";
-		var messageerror = "Vad var det du ville säga?";
-		var i = 0;
-		$("#contact-form").submit(function(){
-			for (i=0;i<required.length;i++) {
-				var input = $('#'+required[i]);
-				if ((input.attr("placeholder") === "") || (input.attr("placeholder") === emptyerror)) {
-					input.addClass("needsfilled");
-					input.attr("placeholder", emptyerror);
-				} else {
-					input.removeClass("needsfilled");
-				}
-			}
-			if ((name.val() === "") || (name.val() === emptyerror)) {
-				name.addClass("needsfilled");
-				name.attr("placeholder", nameerror);
-			}
-			if (!/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/.test(email.val())) {
-				email.addClass("needsfilled");
-				email.attr("placeholder", emailerror);
-			}
-			if ((message.val() === "") || (message.val() === emptyerror)) {
-				message.addClass("needsfilled");
-				message.attr("placeholder", messageerror);
-			}
-			if ($(":input").hasClass("needsfilled")) {
-				return false;
-			} else {
-				return true;
-			}
-		});
-		$("#en-contact-form").submit(function(){
-			for (i=0;i<required.length;i++) {
-				var input = $('#'+required[i]);
-				if ((input.attr("placeholder") === "") || (input.attr("placeholder") === enemptyerror)) {
-					input.addClass("needsfilled");
-					input.attr("placeholder", enemptyerror);
-				} else {
-					input.removeClass("needsfilled");
-				}
-			}
-			if ((name.val() === "") || (name.val() === emptyerror)) {
-				name.addClass("needsfilled");
-				name.attr("placeholder", ennameerror);
-			}
-			if (!/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/.test(email.val())) {
-				email.addClass("needsfilled");
-				email.attr("placeholder", enemailerror);
-			}
-			if ((message.val() === "") || (message.val() === emptyerror)) {
-				message.addClass("needsfilled");
-				message.attr("placeholder", enmessageerror);
-			}
-			if ($(":input").hasClass("needsfilled")) {
-				return false;
-			} else {
-				return true;
-			}
-		});
+
 		$(":input").focus(function(){
 			if ($(this).hasClass("needsfilled") ) {
 				$(this).val("");
 				$(this).removeClass("needsfilled");
 			}
 		});
-		$('input#name').autoResize({
+
+		$("#name, #email").autoResize({
 			minWidth: 320,
 			maxWidth: 600,
 			animate: false
 		});
-		$('input#phone').autoResize({
-			minWidth: 320,
-			maxWidth: 600,
-			animate: false
-		});
-		$('input#email').autoResize({
-			minWidth: 320,
-			maxWidth: 600,
-			animate: false
-		});
-		$('textarea#message').autoResize({
+		$('#message').autoResize({
 			minHeight: 300,
 			maxHeight: 1000,
 			animate: false
@@ -154,6 +100,6 @@ var vincent = {
 };
 
 
-$(document).ready(function() {
+$(document).ready(function () {
 	vincent.init();
 });
