92702e23f5e34dc2a38b1665215c87fb134e47f8
Vincent Orback <vorback@gmail.com>
2014-09-06 14:55:36 +0200

    Removed swedish contact action

--- contactform.php
+++ contactform.php
@@ -17,13 +17,17 @@ $message = "Meddelande från vincentorback.se\n\n" .
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   if ($toemail && $message && $fieldData['email']) {
     mail($toemail, 'Meddelande från vincentorback.se', $message, 'From: ' . $fieldData['email']);
-    echo "Tack för det " . $fieldData['name'] .". <br>Jag återkommer så fort jag bara kan!";
+    $response = "Thank you for your message ". $fieldData['name'] .". <br>I’ll get back to you ASAP!";
   }
   else {
-    echo 'Det blev något fel på ditt meddelande $fieldData["name"]. Försök att skicka ett mail till <a href="mailto:vorback@gmail.com&subject=Message from vincentorback.se&body='.$message.'">vorback@gmail.com</a> istället.';
+    $response = 'Something went wrong. Try sending and email to <a href="mailto:vorback@gmail.com&subject=Message from vincentorback.se&body=' . $fieldData['message'] . '">vorback@gmail.com</a> instead.';
   }
 } else {
-  echo 'Det blev något fel på ditt meddelande $fieldData["name"]. Försök att skicka ett mail till <a href="mailto:vorback@gmail.com&subject=Message from vincentorback.se&body='.$message.'">vorback@gmail.com</a> istället.';
+  $response = 'Something went wrong. Try sending and email to <a href="mailto:vorback@gmail.com&subject=Message from vincentorback.se&body=' . $fieldData['message'] . '">vorback@gmail.com</a> instead.';
 }
+
+
+$data = array('response' => $response);
+echo json_encode($data, JSON_PRETTY_PRINT);
 
 ?>
