fb77e98bd7b6ab690930e03cca84e01cdfe8d1fe
Vincent Orback <vorback@gmail.com>
2015-08-09 20:50:48 +0200

    Added some kind of anti-spam test

--- contactform.php
+++ contactform.php
@@ -2,25 +2,27 @@
 header('Content-Type: application/json; charset=utf-8');
 
 $toemail = 'vorback@gmail.com';
-
+$subject = 'Message from vincentorback.se';
 $fieldData = array(
   'name' => isset($_POST['name']) ? 'Name: ' . $_POST['name'] : false,
   'email' => isset($_POST['email']) ? 'Email: ' . $_POST['email'] : false,
   'message' => isset($_POST['message']) ? "\n" . $_POST['message'] : false);
 
-$text = '';
+$message = '';
 
 foreach ($fieldData as $field) {
   if ($field) {
-    $text = $text . $field . "\n";
+    $message = $message . $field . "\n";
   }
 }
 
-$message = "Message from vincentorback.se\n\n" . $text;
+if ($_POST['spam_test'] != 'foo') {
+  $subject = 'Spam from vincentorback.se';
+}
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   if ($toemail && $message && $_POST['email']) {
-    mail($toemail, 'Message from vincentorback.se', $message, 'From: ' . $_POST['email']);
+    mail($toemail, $subject, $message, 'From: ' . $_POST['email']);
     $response = "Thank you for your message ". $_POST['name'] .". <br>I’ll get back to you ASAP!";
   }
   else {
@@ -30,8 +32,5 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $response = 'Something went wrong. Try sending and email to <a href="mailto:vorback@gmail.com&subject=Message from vincentorback.se&body=' . $_POST['message'] . '">vorback@gmail.com</a> instead.';
 }
 
-
-$data = array('response' => $response);
-
-echo json_encode($data, JSON_PRETTY_PRINT);
+echo json_encode($response, JSON_PRETTY_PRINT);
 ?>
