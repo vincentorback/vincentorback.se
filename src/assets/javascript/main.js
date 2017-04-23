c4ef1dc7bb9d9ba8a3329c0bd008d682bcc06259
vincentorback <vorback@gmail.com>
2017-04-24 00:19:15 +0200

    use mailto links

--- src/assets/javascript/main.js
+++ src/assets/javascript/main.js
@@ -6,17 +6,13 @@
   var supportTouch = Modernizr.touchevents
   var doc = window.document
 
-  function isUndefined (obj) {
-    return (typeof obj === 'undefined')
-  }
-
   var vincent = {
     init: function () {
       if (!supportTouch) {
         vincent.slap()
-
-        vincent.trackLinks()
       }
+
+      vincent.trackLinks()
 
       vincent.contactLink()
     },
@@ -121,16 +117,14 @@
     },
 
     track: function (value, category, callback) {
-      if (isUndefined(ga)) {
-        return
+      if (ga && ga.loaded) {
+        ga('send', {
+          'hitType': 'event',
+          'eventCategory': category,
+          'eventAction': supportTouch ? 'tap' : 'click',
+          'eventValue': value
+        })
       }
-
-      ga('send', {
-        'hitType': 'event',
-        'eventCategory': category,
-        'eventAction': supportTouch ? 'tap' : 'click',
-        'eventValue': value
-      })
 
       if (callback) {
         callback()
@@ -138,35 +132,32 @@
     },
 
     trackLinks: function () {
-      if (isUndefined(ga)) {
-        return
-      }
+      if (ga && ga.loaded) {
+        var outboundLinks = doc.querySelectorAll('a[href^="http"], a[href^="mailto"]')
 
-      var outboundLinks = doc.querySelectorAll('a[href^="http"], a[href^="mailto"]')
-
-      Array.prototype.slice.call(outboundLinks).forEach(function (link) {
-        if (link.host.indexOf('vincentorback') > -1) {
-          return
-        }
-
-        link.addEventListener('click', function (e) {
-          if (vincent.slaps > 0) {
-            vincent.track(vincent.slaps, 'slaps')
-          }
-
-          ga('send', 'event', 'outbound', 'click', link.href, {
-            'transport': 'beacon'
-          })
-
-          if (e.metaKey || e.ctrlKey) {
+        Array.prototype.slice.call(outboundLinks).forEach(function (link) {
+          if (link.host.indexOf('vincentorback') > -1) {
             return
           }
 
-          doc.location = e.currentTarget.href
+          link.addEventListener('click', function (e) {
+            if (vincent.slaps > 0) {
+              vincent.track(vincent.slaps, 'slaps')
+            }
 
-          e.preventDefault()
-        }, false)
-      })
+            ga('send', 'event', 'outbound', 'click', link.href, {
+              'transport': 'beacon',
+              'hitCallback': function () {
+                if (!e.metaKey || !e.ctrlKey) {
+                  doc.location = e.currentTarget.href
+                }
+              }
+            })
+
+            e.preventDefault()
+          }, false)
+        })
+      }
     }
   }
 
