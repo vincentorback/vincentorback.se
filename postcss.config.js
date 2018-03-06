2c649a2d8e408c2c1c4be5f20be8efe66f5d3868
Vincent Orback <vorback@gmail.com>
2018-03-06 16:49:16 +0100

    dont preserve custom properties

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,9 @@
 module.exports = () => ({
   plugins: [
     require('postcss-import')(),
-    require('postcss-custom-properties')(),
+    require('postcss-custom-properties')({
+      preserve: false
+    }),
     require('postcss-custom-media')(),
     require('autoprefixer')(),
     require('postcss-csso')()
