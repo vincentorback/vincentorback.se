5e51d53c0ead582d3d8aca33b395277838648b4a
Vincent Orback <hello@vincentorback.se>
2020-02-04 18:09:29 +0100

    remove service worker

--- src/assets/javascript/main.js
+++ src/assets/javascript/main.js
@@ -332,15 +332,4 @@
   } else {
     doc.addEventListener('DOMContentLoaded', vincent.init, false)
   }
-
-  if (window.location.hostname !== 'localhost' && 'serviceWorker' in navigator) {
-    console.log('CLIENT: service worker registration in progress.')
-    navigator.serviceWorker.register('/assets/javascript/service-worker.js').then(function () {
-      console.log('CLIENT: service worker registration complete.')
-    }, function () {
-      console.log('CLIENT: service worker registration failure.')
-    })
-  } else {
-    console.log('CLIENT: service worker is not supported.')
-  }
 }(window))
